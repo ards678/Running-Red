cf68a58e5e19e7763588acdbd09af2338d87ad26
James <wayperwayp@gmail.com>
2019-05-29 22:55:07 +0800

    moving ground

--- src/Main.cpp
+++ src/Main.cpp
@@ -3,6 +3,7 @@
 #include "background.hpp"
 #include "Obstacles.hpp"
 #include <fstream>
+#include "Ground.h"
 
 using namespace std;
 using namespace sf;
@@ -14,13 +15,25 @@ int main()
     Background bg("content/backdrop.png");
     Background mn("content/static mountain.png");
     Background floor("content/ground.png");
+    Clock clock;
+    // Texture floorTexture;
+
+    // floorTexture.loadFromFile("content/ground.png");
+
     mn.sprite.setPosition(Vector2f(0, 100));
     floor.sprite.setPosition(Vector2f(0,444));
     Background cl("content/cloud1.png");
+    Ground ground1("content/ground.png", Vector2f(0, 440 ), 1);
+    Ground ground2("content/ground.png", Vector2f(1280, 440 ),2);
+    Ground ground3("content/ground.png", Vector2f(2560, 440 ),3);
+    Ground ground4("content/ground.png", Vector2f(3840, 440 ),4);
+
+
 
     SpriteAnimate character("content/playerspritesheet.png", IntRect(0,0,128,128), Vector2f(400,380), 0.05, 1664, 128);
     character.sprite.setOrigin(Vector2f(64,64));
     int score = 0, i = 0, hiScore = 0;
+    int speed = -7;
     float spawn = 2.5;
     Clock game, scr, jumpClock;
     bool gameOn = true;
@@ -74,6 +87,8 @@ int main()
         {
             if (event.type == Event::Closed)
                 window.close();
+             if (Keyboard::isKeyPressed(Keyboard::Key::Escape))
+                window.close();
             if(event.type==Event::KeyPressed && (event.key.code==Keyboard::Up || event.key.code==Keyboard::Space)){
                 character.jump();
                 jump.play();
@@ -88,6 +103,7 @@ int main()
             // if(event.type==Event::KeyPressed && event.key.code==Keyboard::Down)
             //     character.slide();
         }
+    float dt = clock.restart().asMilliseconds()/10;
         if(jumpClock.getElapsedTime().asSeconds() > 0.8f){
             jump.stop();
             jumpClock.restart();
@@ -112,22 +128,32 @@ int main()
         window.clear(Color(240,240,240));
         window.draw(bg.sprite);
         window.draw(mn.sprite);
-        window.draw(floor.sprite);
+        // window.draw(floor.sprite);
         window.draw(character.sprite);
+        window.draw(ground1.drawGround(speed, dt));
+        window.draw(ground2.drawGround(speed, dt));
+        window.draw(ground3.drawGround(speed, dt));
+        window.draw(ground4.drawGround(speed, dt));
+
+        // window.draw();
         for(int k = ob.size()-1; k >= 0; k--){
-            ob[k].update();
-            if(score > 10)
+            ob[k].update(dt);
+            if(score > 10){
                 ob[k].speed = -11;
-            if(score > 20)
+                speed = -11;}
+            if(score > 20){
                 ob[k].speed = -15;
-            if(score > 30)
+                speed = -15;}
+            if(score > 30){
                 ob[k].speed = -19;
+                speed = -19;}
             window.draw(ob[k].sprite);
             if(ob[k].x < 0)
                 ob.erase(ob.begin() + k);
             if(ob[k].isHit(character.sprite)){
                 character.increment = 0;
                 ob[k].speed = 0;
+                speed =-7;
                 gameOn = false;
                 spawn = 2.5;
                 if(scr.getElapsedTime().asSeconds() > 0.05f && i <= score){
@@ -161,4 +187,4 @@ int main()
         window.display();
     }
     return EXIT_SUCCESS;
-}
+};
--- src/Obstacles.hpp
+++ src/Obstacles.hpp
@@ -1,6 +1,6 @@
 #include "Main.hpp"
 #include "Collision.hpp"
-
+#include <cmath>
 using namespace std;
 using namespace sf;
 
@@ -14,11 +14,15 @@ public:
 	int width = 10;
 	int speed = -7;
 	bool isHit(Sprite);
-	void update();
+	void update(float dt);
 	Clock clock;
 };
 
 Obstacle::Obstacle(String s){
+	srand(time(NULL)+speed+width);
+	int rands = 100*rand()%6;
+	x = 1280+ (pow(-1,rands )*rands);
+	cout<<"rands"<<rands<<endl;
 	// srand(time(NULL));
 	// top = (rand() % 3);
 	// String a[] = {"content/obstacle1.png", "content/obstacle2.png", "content/obstacle3.png"};
@@ -34,7 +38,7 @@ bool Obstacle::isHit(Sprite s){
 	return Collision::PixelPerfectTest(sprite, s);
 }
 
-void Obstacle::update(){
-	x += speed;
+void Obstacle::update(float dt){
+	x += speed*dt;
 	sprite.setPosition(x, 444);
 }
